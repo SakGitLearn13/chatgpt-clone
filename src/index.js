1ba5c106dd7ab12e914e25ecf6f08445bd786413
SakLearn <SakLearn@1312@>
2024-07-04 15:27:12 +0530

    Test Commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import App from './App';
+
+
+const root = React.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
